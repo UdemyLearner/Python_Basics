b4837ed38716a46d180d18af342a9a2c70a6eaaa
Priyanshu_Naredi <shellrdphacks@gmail.com>
2022-07-16 13:51:24 +0530

    new file:   Day 5/PasswordGenrator.py 	modified:   Day 7/Hangman.py

--- Day 7/Hangman.py
+++ Day 7/Hangman.py
@@ -1,2 +1,91 @@
+from dis import dis
+from math import fabs
+from operator import truediv
+import random
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
 word_list=["ardvak","baboon","camel"]
-print(random)
+end_of_game=False                                                          # end_of_game=0
+lives=6
+chosen_word=random.choice(word_list)
+print(chosen_word)
+word_length=len(chosen_word)
+
+display=[]
+for _ in range(word_length):
+    display+="_"
+print(display)
+  
+while not end_of_game:                                                      # while end_of_game<word_length:
+    guess = input("Guess a letter: ").lower()
+                                                                            # end_of_game+=1
+    boll=False
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+                display[position] = letter       
+                boll=True  
+    print(display)    
+    if boll==False:
+        lives-=1
+        print(f"No. of lifes Reamning are {lives}")   
+        print(display)
+        print(stages[lives])
+        if lives==0:
+            end_of_game=True
+
