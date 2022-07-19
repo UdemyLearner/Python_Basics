49cb46df9a2c1e04c8996491014f5c1daad458e3
Priyanshu_Naredi <shellrdphacks@gmail.com>
2022-07-20 00:23:45 +0530

    Changes to be committed: 	new file:   Day 8/__pycache__/art.cpython-310.pyc 	new file:   Day 8/art.py 	modified:   Day 8/caesar_cipher.py

--- Day 8/caesar_cipher.py
+++ Day 8/caesar_cipher.py
@@ -1,21 +1,69 @@
+from art import logo
+print(logo)
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
 
 def ceasar(start_text,shift_amount,cipher_direction):
     end_text=""
     if cipher_direction=="decode":
         shift_amount*=-1
-    for letter in start_text:
-        position=alphabet.index(letter)
-        new_position=position+shift_amount
-        end_text+=alphabet[new_position]
+    for char in start_text:
+        if char in alphabet:
+            position=alphabet.index(char)
+            new_position=position+shift_amount
+            end_text+=alphabet[new_position]
+        else:
+            end_text+=char
     print(f"The {cipher_direction} text is {end_text}")
      
     
-ceasar(text,shift,direction)
+should_continue=True 
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift%=26
+    ceasar(text,shift,direction)
+    result=input("do you want to continue yes or no \n")
+    if result=="no":
+        should_continue=False
+        print("GoodBye")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
