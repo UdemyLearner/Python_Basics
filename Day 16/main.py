a434ad1fda53efca4d533c49f28aa78c9cbd2fda
Priyanshu_Naredi <shellrdphacks@gmail.com>
2022-10-23 20:55:33 +0530

    Changes to be committed: 	modified:   Day 16/main.py

--- Day 16/main.py
+++ Day 16/main.py
@@ -1,11 +1,11 @@
-import turtle
+import turtle as tuktuk
 
-tuktuk = turtle.Turtle()
+                             #tuktuk = turtle.Turtle()
 print(tuktuk)
 tuktuk.shape("turtle")
 tuktuk.clone()
 tuktuk.forward(100.0)
 
-my_screen = turtle.Screen()
+my_screen = tuktuk.Screen()  # my_screen = turtle.Screen()
 print(my_screen.canvheight)
 my_screen.exitonclick()
