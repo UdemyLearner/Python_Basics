a9d3becf1979a5d0cbeed59ba5d5059e3df36a8d
Priyanshu_Naredi <shellrdphacks@gmail.com>
2022-11-12 16:39:01 +0530

    Changes to be committed: 	modified:   Day 17/Quiz Project/__pycache__/quiz_brain.cpython-310.pyc 	modified:   Day 17/Quiz Project/quiz_brain.py

--- Day 17/Quiz Project/quiz_brain.py
+++ Day 17/Quiz Project/quiz_brain.py
@@ -1,20 +1,19 @@
-
 class QuizBrain:
-    def __init__(self,q_list):
+    def __init__(self, q_list):
         self.question_number = 0
         self.score = 0
         self.question_list = q_list
-        
+
     def still_has_question(self):
         return self.question_number < len(self.question_list)
-        
+
     def nxt_question(self):
         current_ques = self.question_list[self.question_number]
-        self.question_number +=1
+        self.question_number += 1
         u_ans = input(f"Q.{self.question_number} {current_ques.text} (True/False) :")
-        self.check_ans(u_ans,current_ques.answer)
-        
-    def check_ans(self,u_ans,ans):
+        self.check_ans(u_ans, current_ques.answer)
+
+    def check_ans(self, u_ans, ans):
         if u_ans.lower() == ans.lower():
             self.score += 1
             print("correct ans")
